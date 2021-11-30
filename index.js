5a65fef89c90377947d03ea493bb0b531281a727
Nicholas Alayra <nalayra@hotmail.com>
2021-11-30 10:46:55 -0800

    commit tree ced2269f317ff54cb73f7ed778915f82b03cc2b4

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const input = document.getElementById('input');
+
 function addingEventListener() {
-    const input = document.getElementById('input');
-    input.addEventListener('click', function () {
+    input.addEventListener('click', function (event) {
         alert('I was clicked!');
     });
 }
+// const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
